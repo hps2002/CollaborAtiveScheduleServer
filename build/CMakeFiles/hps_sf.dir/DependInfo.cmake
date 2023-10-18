c13344017f68b7e9bbbd9e3eea396601235e42be
hps2002 <1669738451@qq.com>
2023-10-18 14:15:08 +0800

    feat(*) 初步开始hook模块

---
+++ build/CMakeFiles/hps_sf.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/hps_sf/hps_sf/config.cpp" "CMakeFiles/hps_sf.dir/hps_sf/config.cpp.o" "gcc" "CMakeFiles/hps_sf.dir/hps_sf/config.cpp.o.d"
+  "/home/ubuntu/hps_sf/hps_sf/fiber.cpp" "CMakeFiles/hps_sf.dir/hps_sf/fiber.cpp.o" "gcc" "CMakeFiles/hps_sf.dir/hps_sf/fiber.cpp.o.d"
+  "/home/ubuntu/hps_sf/hps_sf/hook.cpp" "CMakeFiles/hps_sf.dir/hps_sf/hook.cpp.o" "gcc" "CMakeFiles/hps_sf.dir/hps_sf/hook.cpp.o.d"
+  "/home/ubuntu/hps_sf/hps_sf/iomanager.cpp" "CMakeFiles/hps_sf.dir/hps_sf/iomanager.cpp.o" "gcc" "CMakeFiles/hps_sf.dir/hps_sf/iomanager.cpp.o.d"
+  "/home/ubuntu/hps_sf/hps_sf/log.cpp" "CMakeFiles/hps_sf.dir/hps_sf/log.cpp.o" "gcc" "CMakeFiles/hps_sf.dir/hps_sf/log.cpp.o.d"
+  "/home/ubuntu/hps_sf/hps_sf/scheduler.cpp" "CMakeFiles/hps_sf.dir/hps_sf/scheduler.cpp.o" "gcc" "CMakeFiles/hps_sf.dir/hps_sf/scheduler.cpp.o.d"
+  "/home/ubuntu/hps_sf/hps_sf/thread.cpp" "CMakeFiles/hps_sf.dir/hps_sf/thread.cpp.o" "gcc" "CMakeFiles/hps_sf.dir/hps_sf/thread.cpp.o.d"
+  "/home/ubuntu/hps_sf/hps_sf/timer.cpp" "CMakeFiles/hps_sf.dir/hps_sf/timer.cpp.o" "gcc" "CMakeFiles/hps_sf.dir/hps_sf/timer.cpp.o.d"
+  "/home/ubuntu/hps_sf/hps_sf/util.cpp" "CMakeFiles/hps_sf.dir/hps_sf/util.cpp.o" "gcc" "CMakeFiles/hps_sf.dir/hps_sf/util.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test_timer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/ubuntu/hps_sf/bin/test_timer"
+  "/home/ubuntu/hps_sf/bin/test_timer.pdb"
+  "CMakeFiles/test_timer.dir/tests/test_timer.cpp.o"
+  "CMakeFiles/test_timer.dir/tests/test_timer.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_timer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
